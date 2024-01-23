e986931a9301f9b638f2ff201e2435ecc6854b7b
rrcsmehta3 <smehta3@academic.rrc.ca>
2024-01-22 21:22:12 -0600

    Initial commit.  Documented project.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Module 02 Demonstration 
+
+## Description 
+Introduction to Python: Documentation, Variables, Printing
+
+
+## Author
+Sapana Mehta
---
+++ module_02.py
@@ -0,0 +1,8 @@
+"""
+Description: Module 02 demonstration
+Author: Sapana Mehta
+Date: 22-02-2024
+Usage: To demonstrate content from module 02
+
+"""
+
