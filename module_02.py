566011355acc22bf74524e3291d6c5b85d058acb
rrcsmehta3 <smehta3@academic.rrc.ca>
2024-01-23 23:29:15 -0600

    Experimented with built-in funcations.

--- module_02.py
+++ module_02.py
@@ -6,3 +6,27 @@ Usage: To demonstrate content from module 02
 
 """
 
+absolute_value = abs (-12)
+print ('absolute value:' , absolute_value)
+
+# Funcation embedded within another function.
+print('absolute value:' , abs(-12))
+
+print("Hello World!")
+
+print ("I am", 19, "years old.")
+
+print("apples","oranges","bananas",sep=",")
+
+name = "Sapana"
+age = 19
+print(f"My name is {name} and I m {age} years old.")
+
+value = 3.14159
+print(f"The value is {value:.2f}.")
+
+number =123
+print(f"The number is {number:05}.")
+
+name = "Sapana"
+print(f"Hello, {name:>10}.")
