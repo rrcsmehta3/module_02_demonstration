5554874cc465e1f2956ae3b88ffbe199d6e1ffb9
rrcsmehta3 <smehta3@academic.rrc.ca>
2024-01-25 16:36:59 -0600

    simple data types

---
+++ sdf_assignment_2.py
@@ -0,0 +1,25 @@
+
+
+#SIMPLE DATA TYPES
+name = "Sapana"
+print("Value: " + name + "  Type: ", type(name))
+
+license ="True"    
+print("Value: " + license + " Type:" , type(True))
+
+Year = "2024"
+print("this year: " + Year+ " Type:" , type(2024))
+
+Year = "2025"
+print("next year: " +Year + " Type: " , type(2025))
+
+#CALCULATIONS
+
+#LISTS
+
+#TUPLES
+
+#DICTIONARIES
+
+#SETS
+
